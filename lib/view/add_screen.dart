a145be999506cede34b0263cd5958d7c0031e646
AfilAhamed <afilahamed0007@gmail.com>
2023-11-11 12:46:16 +0530

    first commit

--- lib/view/add_screen.dart
+++ lib/view/add_screen.dart
@@ -7,7 +7,53 @@ class AddScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add'),
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.teal.shade400,
+        title: const Text(
+          'AddScreen',
+          style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: [
+          TextFormField(
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.teal.shade400)),
+                  hintText: 'Title',
+                  border: const OutlineInputBorder())),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 8,
+              minLines: 4,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 3, color: Colors.teal.shade400)),
+                  hintText: 'Description',
+                  border: const OutlineInputBorder())),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  backgroundColor: Colors.teal.shade400),
+              onPressed: () {},
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 23,
+                ),
+              ))
+        ]),
       ),
     );
   }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -6,36 +6,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.teal.shade400,
-            title: const Text(
-              'HomeScreen',
-              style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-            )),
-        body: const Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Column(children: []),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton.extended(
+    return Scaffold(
+      appBar: AppBar(
           backgroundColor: Colors.teal.shade400,
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AddScreen(),
-                ));
-          },
-          label: const Text('Add',
-              style: TextStyle(color: Colors.white, fontSize: 20)),
-          icon: const Icon(
-            Icons.add,
-            color: Colors.white,
-            size: 29,
-          ),
+          title: const Text(
+            'HomeScreen',
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+          )),
+      body: const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.teal.shade400,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddScreen(),
+              ));
+        },
+        label: const Text('Create',
+            style: TextStyle(color: Colors.white, fontSize: 20)),
+        icon: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 29,
         ),
       ),
     );
