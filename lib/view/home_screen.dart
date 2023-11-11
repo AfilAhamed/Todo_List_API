b5778d4d514a4e6c12be1a196e7dcc8dd87b6fe8
AfilAhamed <afilahamed0007@gmail.com>
2023-11-11 11:44:16 +0530

    first commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_api/view/add_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,11 +10,33 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.teal.shade400,
-            title: Text(
+            title: const Text(
               'HomeScreen',
               style:
-                  TextStyle(fontWeight: FontWeight.w600, color: Colors.black87),
+                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
             )),
+        body: const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(children: []),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: Colors.teal.shade400,
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddScreen(),
+                ));
+          },
+          label: const Text('Add',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+          icon: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 29,
+          ),
+        ),
       ),
     );
   }
